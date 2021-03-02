ec989f615efa942c6c531a94ebbce39092087338
tadeuasarro <tadeutas@gmail.com>
2021-03-02 19:20:04 -0300

    Add relations between models

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -4,4 +4,6 @@ class Expenditure < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 100 }
 
   validates :date, presence: true, length: { is: 10 }
+
+  belongs_to :expense
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+
+  has_many :expenditures
 end

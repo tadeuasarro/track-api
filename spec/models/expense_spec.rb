a9213df9d8895d5d11af34be0e5171b8d361a6c7
tadeuasarro <tadeutas@gmail.com>
2021-03-02 18:19:58 -0300

    Add some testcases for expenses model

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe Expense, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+
+    it "expects the subject to be invalid for it doesn't have an username" do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "expects the subject to me invalid for it's length is not enough" do
+      subject.name = '123'
+      expect(subject).not_to be_valid
+    end
+
+    it "expects the subject to me invalid for it's length is not enough" do
+      subject.name = '123456789012345678901234567890'
+      expect(subject).not_to be_valid
+    end
+
   end
 end

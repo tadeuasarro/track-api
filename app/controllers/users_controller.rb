359fc550dab9b6eff3f421f79b1dc939276ffe78
tadeuasarro <tadeutas@gmail.com>
2021-03-03 19:40:21 -0300

    Finish work for the day, login feature working with HTTP request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:id])
 
-    Rails.logger.debug params
-
     respond_to do |format|
       format.json { render :json => @user }
     end

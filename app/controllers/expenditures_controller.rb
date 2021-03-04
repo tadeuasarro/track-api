a23337c8827edab330fa0ccc6c739c50752d6b48
tadeuasarro <tadeutas@gmail.com>
2021-03-04 13:37:16 -0300

    Update and clean expenditures routes and controller for now

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -4,27 +4,8 @@ class ExpendituresController < ApplicationController
     @expenditures = Expenditure.all
 
     respond_to do |format|
-      format.html
       format.json { render :json => @expenditures }
     end
-  end
-
-  def show
-    @expenditure = Expenditure.find(params[:id])
-  end
-
-  def create
-    expenditure = Expenditure.new(expenditure_params)
-
-    Rails.logger.debug params
-  end
-
-  def update
-    expenditure = Expenditure.find(params[:id])
-  end
-
-  def destroy
-    expenditure = Expenditure.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :expenditures
+  resources :expenditures, only: [:index]
   resources :users, only: [:show]
 end

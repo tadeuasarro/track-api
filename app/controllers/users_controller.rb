f2dd7a51ff90fc1f614030abb94d339dc7a73d2b
tadeuasarro <tadeutas@gmail.com>
2021-03-19 16:43:29 -0300

    Finish login feature for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: true, status: :ok
+      render json: user, status: :ok
     else
       render json: user.errors, status: :unprocessable_entity
     end

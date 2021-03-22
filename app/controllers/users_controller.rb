d8d28b791cbafd13b09451db2034fb8c332dd617
tadeuasarro <tadeutas@gmail.com>
2021-03-22 16:53:25 -0300

    Add join table to user create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: user, status: :ok
+      render json: user.to_json(include: :expenditures), status: :ok
     else
       render json: user.errors, status: :unprocessable_entity
     end

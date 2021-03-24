bee6ae75553916b8710ebcc062e4913fa4547285
tadeuasarro <tadeutas@gmail.com>
2021-03-24 14:10:42 -0300

    Try and fix API accoding to review

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -7,9 +7,9 @@ class ExpendituresController < ApplicationController
 
   def create
     expenditure = Expenditure.new(expenditure_params)
-    expenditures = Expenditure.fetch_expenditures(params[:user_id])
 
     if expenditure.save
+      expenditures = Expenditure.fetch_expenditures(params[:user_id])
       render json: expenditures, status: :ok
     else
       render json: expenditure.errors, status: :unprocessable_entity
@@ -25,7 +25,7 @@ class ExpendituresController < ApplicationController
   private
 
   def expenditure_params
-    params.require(:expenditure).permit(:expense_id, :value, :date, :description, :user_id)
+    params.require(:expenditure).permit(:category, :value, :date, :description, :user_id)
   end
 
   def user_params
